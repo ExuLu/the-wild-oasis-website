5459ff8f0f7606a87de181623b4ed2f3cca71388
Alena Sharai <exultantislupus@gmail.com>
2025-10-02 17:51:14 +0100

    feat: create layout and add logo

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Navigation from './components/Navigation';
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise</h1>
     </div>
   );
