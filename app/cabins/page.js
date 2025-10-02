592c99d1ecdc1cb186c7b0a621639a801100c6c8
Alena Sharai <exultantislupus@gmail.com>
2025-10-02 18:40:37 +0100

    feat: add client component Counter

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,13 @@
-import Navigation from '../components/Navigation';
+import Counter from '../components/Counter';
 
 const page = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
 
-  console.log(data);
-
   return (
     <div>
       <h1>Cabins page</h1>
+      <Counter users={data} />
 
       <ul>
         {data.map((user) => (
