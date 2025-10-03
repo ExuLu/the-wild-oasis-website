7b159b678a1218ee0e94be97b2380fdd7f1c394b
Alena Sharai <exultantislupus@gmail.com>
2025-10-03 23:52:11 +0100

    feat: add layout for account page

---
+++ app/account/layout.js
@@ -0,0 +1,10 @@
+const layout = ({ children }) => {
+  return (
+    <div className='grid grid-cols-[16rem_1fr] h-full gap-12'>
+      <div>Navigation</div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default layout;
