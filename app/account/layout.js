d71e03ea850b431fd3170b218f468e81311f9176
Alena Sharai <exultantislupus@gmail.com>
2025-10-04 00:00:56 +0100

    feat: add SideNavigation to the accout layout

--- app/account/layout.js
+++ app/account/layout.js
@@ -1,8 +1,12 @@
+import SideNavigation from '@/app/_components/SideNavigation';
+
 const layout = ({ children }) => {
   return (
     <div className='grid grid-cols-[16rem_1fr] h-full gap-12'>
-      <div>Navigation</div>
-      <div className="py-1">{children}</div>
+      <div>
+        <SideNavigation />
+      </div>
+      <div className='py-1'>{children}</div>
     </div>
   );
 };
