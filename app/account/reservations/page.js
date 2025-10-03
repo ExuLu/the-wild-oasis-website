58c8c918f88d9140da33692acf27a161e51cd0bc
Alena Sharai <exultantislupus@gmail.com>
2025-10-04 00:33:37 +0100

    feat: add metadate to profile and reservations pages

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,5 +1,9 @@
 import ReservationCard from '@/app/_components/ReservationCard';
 
+export const metadata = {
+  title: 'Reservations',
+};
+
 const page = () => {
   // CHANGE
   const bookings = [];
