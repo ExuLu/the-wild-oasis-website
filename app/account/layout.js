f7ea9921d0242788a2e9aa4f750178f3ac016ba9
Alena Sharai <exultantislupus@gmail.com>
2025-10-04 00:25:55 +0100

    fix: adjust styles and markup

--- app/account/layout.js
+++ app/account/layout.js
@@ -3,9 +3,7 @@ import SideNavigation from '@/app/_components/SideNavigation';
 const layout = ({ children }) => {
   return (
     <div className='grid grid-cols-[16rem_1fr] h-full gap-12'>
-      <div>
-        <SideNavigation />
-      </div>
+      <SideNavigation />
       <div className='py-1'>{children}</div>
     </div>
   );
