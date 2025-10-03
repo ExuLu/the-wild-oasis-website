08e278bb358303bdd832524b7d4f9a7a5c063a1f
Alena Sharai <exultantislupus@gmail.com>
2025-10-03 23:08:51 +0100

    fix: adjust markdow

--- app/about/page.js
+++ app/about/page.js
@@ -30,47 +30,47 @@ const page = () => {
             feel the joy of being together in a beautiful setting.
           </p>
         </div>
+      </div>
 
-        <div className='col-span-2'>
-          <img
-            src='/about-1.jpg'
-            alt='Family sitting around a fire pit in front of cabin'
-          />
-        </div>
+      <div className='col-span-2'>
+        <img
+          src='/about-1.jpg'
+          alt='Family sitting around a fire pit in front of cabin'
+        />
+      </div>
 
-        <div className='col-span-2'>
-          <img src='/about-2.jpg' alt='Family that manages The Wild Oasis' />
-        </div>
+      <div className='col-span-2'>
+        <img src='/about-2.jpg' alt='Family that manages The Wild Oasis' />
+      </div>
 
-        <div className='col-span-3'>
-          <h1 className='text-4xl mb-10 text-accent-400 font-medium'>
-            Managed by our family since 1962
-          </h1>
+      <div className='col-span-3'>
+        <h1 className='text-4xl mb-10 text-accent-400 font-medium'>
+          Managed by our family since 1962
+        </h1>
 
-          <div className='space-y-8'>
-            <p>
-              Since 1962, The Wild Oasis has been a cherished family-run
-              retreat. Started by our grandparents, this haven has been nurtured
-              with love and care, passing down through our family as a testament
-              to our dedication to creating a warm, welcoming environment.
-            </p>
-            <p>
-              Over the years, we&apos;ve maintained the essence of The Wild
-              Oasis, blending the timeless beauty of the mountains with the
-              personal touch only a family business can offer. Here, you&apos;re
-              not just a guest; you&apos;re part of our extended family. So join
-              us at The Wild Oasis soon, where tradition meets tranquility, and
-              every visit is like coming home.
-            </p>
+        <div className='space-y-8'>
+          <p>
+            Since 1962, The Wild Oasis has been a cherished family-run retreat.
+            Started by our grandparents, this haven has been nurtured with love
+            and care, passing down through our family as a testament to our
+            dedication to creating a warm, welcoming environment.
+          </p>
+          <p>
+            Over the years, we&apos;ve maintained the essence of The Wild Oasis,
+            blending the timeless beauty of the mountains with the personal
+            touch only a family business can offer. Here, you&apos;re not just a
+            guest; you&apos;re part of our extended family. So join us at The
+            Wild Oasis soon, where tradition meets tranquility, and every visit
+            is like coming home.
+          </p>
 
-            <div>
-              <a
-                href='/cabins'
-                className='inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'
-              >
-                Explore our luxury cabins
-              </a>
-            </div>
+          <div>
+            <a
+              href='/cabins'
+              className='inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'
+            >
+              Explore our luxury cabins
+            </a>
           </div>
         </div>
       </div>
