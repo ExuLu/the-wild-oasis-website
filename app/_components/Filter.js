b3e1823038e273a7d91b3b74f369246beb612e2d
Alena Sharai <exultantislupus@gmail.com>
2025-10-07 22:46:31 +0100

    refactor: add button array and loop throug it in Filter component

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -2,37 +2,30 @@
 
 import useSetSearchParams from '@/app/_hooks/useSetSearchParams';
 import { useSearchParams } from 'next/navigation';
+import FilterButton from './FilterButton';
 
 const Filter = () => {
   const { currentFilterValue, handleFilter } = useSetSearchParams('capacity');
   const activeFilter = currentFilterValue ?? 'all';
+  const buttons = [
+    { value: 'all', label: 'All cabins' },
+    { value: 'small', label: '1 - 3 guests' },
+    { value: 'medium', label: '4 - 7 guests' },
+    { value: 'large', label: '8 - 12 guests' },
+  ];
 
   return (
     <div className='border border-primary-800 flex'>
-      <button
-        className='px-5 py-2 hover:bg-primary-700'
-        onClick={() => handleFilter('all')}
-      >
-        All cabins
-      </button>
-      <button
-        className='px-5 py-2 hover:bg-primary-700'
-        onClick={() => handleFilter('small')}
-      >
-        1&mdash;3 guests
-      </button>
-      <button
-        className='px-5 py-2 hover:bg-primary-700'
-        onClick={() => handleFilter('medium')}
-      >
-        4&mdash;7 guests
-      </button>
-      <button
-        className='px-5 py-2 hover:bg-primary-700'
-        onClick={() => handleFilter('large')}
-      >
-        8&mdash;12 guests
-      </button>
+      {buttons.map((button) => (
+        <FilterButton
+          activeFilter={activeFilter}
+          filter={button.value}
+          handleFilter={handleFilter}
+          key={button.value}
+        >
+          {button.label}
+        </FilterButton>
+      ))}
     </div>
   );
 };
