d21df1cc6968cbbaf22e87975ee5aab0b59cc7ab
Alena Sharai <exultantislupus@gmail.com>
2025-10-09 16:10:42 +0100

    feat: add user image to app

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 
-const Navigation = () => {
+import { auth } from '@/app/_lib/auth';
+import Image from 'next/image';
+
+const Navigation = async () => {
+  const session = await auth();
+  console.log(session?.user?.image);
+
   return (
     <nav className='z-10 text-xl'>
       <ul className='flex gap-16 items-center'>
@@ -21,12 +27,29 @@ const Navigation = () => {
           </Link>
         </li>
         <li>
-          <Link
-            className='hover:text-accent-400 transition-colors'
-            href='/account'
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              className='hover:text-accent-400 transition-colors flex items-center gap-4'
+              href='/account'
+            >
+              <div className='h-8 relative w-8'>
+                <Image
+                  alt={session.user.name}
+                  className='object-cover rounded-full'
+                  fill
+                  src={session?.user?.image}
+                />
+              </div>
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              className='hover:text-accent-400 transition-colors'
+              href='/account'
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
