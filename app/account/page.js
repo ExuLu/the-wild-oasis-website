4672b1d085797f3b9ae3f1e7751e88c89dc65be4
Alena Sharai <exultantislupus@gmail.com>
2025-10-09 17:54:48 +0100

    feat: add user data to account page

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from '@/app/_lib/auth';
+
 export const metadata = {
   title: 'Guest Area',
 };
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth();
+  const firstName = session.user.name.split(' ').at(0);
+
   return (
     <h1 className='font-semibold text-2xl text-accent-400 mb-7'>
-      Welcome, Alena
+      Welcome, {firstName}
     </h1>
   );
 };
