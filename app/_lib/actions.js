1c6d5baa9e6c817eacbcdf757d91be3f971856cc
Alena Sharai <exultantislupus@gmail.com>
2025-10-14 14:57:28 +0100

    feat: get form data in server function

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,9 +1,14 @@
 'use server';
 
-import { signIn, signOut } from './auth';
+import { auth, signIn, signOut } from './auth';
 
 export const updateGuest = async (formData) => {
-  console.log(formData);
+  const session = await auth();
+
+  if (!session) throw Error('You must be logged in');
+
+  const nationalID = formData.get('nationalID');
+  const nationalityData = formData.get('nationality').split('%');
 };
 
 export const signInAction = async () => {
